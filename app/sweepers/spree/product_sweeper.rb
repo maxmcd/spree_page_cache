6d7360fb30e9a6460580498fd13a6bbe3c0536ae
Max McDonnell <max.t.mcdonnell@gmail.com>
2013-05-01 19:37:57 -0400

    add taxons to product sweeper

--- app/sweepers/spree/product_sweeper.rb
+++ app/sweepers/spree/product_sweeper.rb
@@ -5,6 +5,7 @@ module Spree
     def after_save(product)
         expire_action(:controller => "/spree/home", :action => "index")
         expire_action(:controller => "/spree/products", :action => "index")
+        expire_action(:controller => "/spree/taxons", :action => "show")
         expire_action(:controller => "/spree/products", :action => "show", :id => product)
         # FileUtils.rm_rf "#{page_cache_directory}/t"
     end
